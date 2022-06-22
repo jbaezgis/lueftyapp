1193738965802d163a22c55cc2f3c474c6ec02a3
Yoel Baez <jbaezgis@gmail.com>
2022-06-22 14:11:30 -0400

    Filters bug fixed - 1.0.07

--- resources/views/livewire/won-offers.blade.php
+++ resources/views/livewire/won-offers.blade.php
@@ -15,11 +15,11 @@
                 <div>
                     <div class="text-xs text-center text-gray-500">Filters</div>
                     <div class="flex justify-center text-gray-600">
-                        <a href="#" wire:click="openModal()">
+                        <button type="button" wire:click="openModal()">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
                             </svg>
-                        </a>
+                        </button>
                     </div>
                 </div>
             </div>
